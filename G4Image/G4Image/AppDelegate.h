ace86f17480cedc7eb1c09464f341bc0bf6259a9
LiuHuiYang <liuhuiyang2009@163.com>
2017-08-03 18:03:54 +0800

    创建工程

---
+++ G4Image/G4Image/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  G4Image
+//
+//  Created by LHY on 2017/8/3.
+//  Copyright © 2017年 SmartHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ G4Image/G4Image/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  G4Image
+//
+//  Created by LHY on 2017/8/3.
+//  Copyright © 2017年 SmartHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
